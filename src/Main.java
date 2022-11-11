b70080c5feb8eefc0c16c2a8301f371f42a0bae6
Thiago <thiago.bohonos@gmail.com>
2022-11-11 01:29:28 -0300

    Atualização do sistema

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int pontos = 0;
+        System.out.println("Bem-vindo! Pressione S para continuar.");
+        String string = scanner.nextLine();
+        if(string.equals("s")) {
+            System.out.println("Qual a capital do Brasil?");
+            string = scanner.nextLine();
+            if(string.equals("Brasília")) {
+                pontos++;
+                System.out.println("Você pontuou!");
+                System.out.println("Você deseja continuar? (s) ou desistir? (d)");
+                string = scanner.nextLine();
+                if(string.equals("s")) {
+                    //Próxima pergunta.
+                    System.out.println("Em qual continente fica o Brasil?");
+                    string = scanner.nextLine();
+                    if(string.equals("América do Sul")) {
+                        pontos++;
+                        System.out.println("Você pontuou!");
+                        System.out.println("Você deseja continuar? (s) ou desistir? (d)");
+                        string = scanner.nextLine();
+                        if(string.equals("s")) {
+                            System.out.println("Qual a língua oficial do Brasil?");
+                            string = scanner.nextLine();
+                            if(string.equals("português")) {
+                                pontos++;
+                                System.out.println("Você ganhou!");
+                                System.out.println("Sua pontuação final foi de: " + pontos);
+                            }else {
+                                System.out.println("Game over! Sua pontuação foi de: " + pontos);
+                            }
+                        }else {
+                            System.out.println("Game over! Sua pontuação foi de: " + pontos);
+                        }
+                    }else {
+                        System.out.println("Game over! Sua pontuação foi de: " + pontos);
+                    }
+                }else {
+                    System.out.println("Game over! Sua pontuação foi de: " + pontos);
+                }
+            }else {
+                System.out.println("Game over! Sua pontuação foi de: " + pontos);
+            }
+        }else {
+            System.out.println("Acabou o jogo... Você pontuou 0");
+        }
     }
 }
